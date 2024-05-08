676b4f2bec5883e75237e4be9b4a78b5eae85794
Khaleed Opeloyeru <khaleedopeloyerudev@gmail.com>
2024-05-08 10:34:40 -0400

    working fine

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """
 todo application
 """
+import sys
 import utils
 import json
 from enum import Enum
@@ -8,6 +9,8 @@ from uuid import uuid4
 from prettytable import PrettyTable
 task_table = PrettyTable()
 task_table.field_names = ["Task", "Priority", "Id"]
+
+tasks = []
 
 
 class Priority(Enum):
@@ -24,16 +27,35 @@ class TaskItem:
 
 
 def load_tasks_from_file():
-    tasks = []
-    task_file = open("tasks.json", "r")
-    task_data = json.load(task_file)
-    for task in task_data:
-        task_item = TaskItem(task["name"], task["priority"], task["id"])
-        tasks.append(task_item)
-    return tasks
+    # default_task = TaskItem("Welcome to Taskpulse",
+    #                         Priority.HIGH.value, uuid4())
+    # tasks = [default_task]
+    try:
+        task_file = open("tasks.json", "r")
+        task_data = json.load(task_file)
+        for task in task_data:
+            task_item = TaskItem(task["name"], task["priority"], task["id"])
+            tasks.append(task_item)
+    except Exception as e:
+        print(f"error {e} occurred")
+    finally:
+        return tasks
 
 
-tasks = load_tasks_from_file()
+def save_tasks_to_file() -> None:
+    task_dictionary_list = []
+    for task in tasks:
+        task_dict = task.__dict__.copy()
+        task_dict["id"] = str(task_dict["id"])
+        task_dictionary_list.append(task_dict)
+    task_file = open("tasks.json", "w")
+    json.dump(task_dictionary_list, task_file)
+
+
+def initial_setup() -> None:
+    global tasks
+    tasks = load_tasks_from_file()
+    save_tasks_to_file()
 
 
 def display_welcome_message() -> None:
@@ -61,6 +83,7 @@ def add_task() -> None:
         id = uuid4()
         task = TaskItem(name, priority, id)
         tasks.append(task)
+    save_tasks_to_file()
     print("task(s) successfully added")
 
 
@@ -72,21 +95,25 @@ def display_menu() -> None:
     print("1. Display tasks")
     print("2. Create a task")
     print("3. Delete a task")
+    print("4. Exit")
 
 
 def run_menu() -> None:
     display_menu()
     user_selection = get_user_selection()
     utils.clear_console()
-    if (user_selection == 1):
+    if user_selection == 1:
         display_tasks()
-    elif (user_selection == 2):
+    elif user_selection == 2:
         add_task()
-    else:
+    elif user_selection == 3:
         delete_task()
+    elif user_selection == 4:
+        sys.exit()
 
 
 def main() -> None:
+    initial_setup()
     display_welcome_message()
     while True:
         run_menu()
